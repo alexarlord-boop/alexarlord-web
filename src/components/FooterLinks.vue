adada82e13157309ec8b4cbb1507e4cb5aeaaa09
alexarlord-boop <rezerw0.1@yandex.ru>
2023-02-12 22:20:39 +0200

    WEB-2023 Added footer links + button for more

---
+++ src/components/FooterLinks.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="links">
+   <slot></slot>
+  </div>
+</template>
+
+<script>
+import FooterLink from "@/components/FooterLink.vue";
+
+export default {
+  name: "FooterLinks",
+  components: {FooterLink}
+}
+</script>
+
+<style lang="sass">
+
+
+</style>
