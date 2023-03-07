88a2b69ff84b92cfede591104a430f710b60b663
alexarlord-boop <rezerw0.1@yandex.ru>
2023-03-07 17:01:37 +0200

    WEB-2023 Added dist

--- src/main.js
+++
@@ -1,9 +0,0 @@
-import "bootstrap/dist/css/bootstrap.css"
-import { createApp } from 'vue'
-import App from './App.vue'
-
-import './assets/main.sass'
-
-createApp(App).mount('#app')
-
-import "bootstrap/dist/js/bootstrap"
