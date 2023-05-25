966af7d55bd95d0ed580d8b5c69d1f83c20a66b2
alexarlord-boop <rezerw0.1@yandex.ru>
2023-05-25 18:47:28 +0300

    WEB-2023 Updated navbar - links, icons

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,5 +1,5 @@
 <template>
-  <nav class="navbar navbar-expand-lg bg-body-tertiary">
+  <nav class="navbar navbar-expand-lg bg-body-tertiary" style="box-shadow: rgba(225,225,203,0.58) 2px 5px 20px">
     <div class="container-fluid">
       <a class="navbar-brand" href="#">alexarlord |</a>
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -7,22 +7,25 @@
       </button>
       <div class="collapse navbar-collapse" id="navbarSupportedContent">
         <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-          <li class="nav-item">
-            <a class="nav-link" aria-current="page" href="https://github.com/alexarlord-boop">GitHub</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="https://www.linkedin.com/in/aleksandr-petrunin-b4b720259/">LinkedIn</a>
-          </li>
+
           <li class="nav-item dropdown">
             <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-              Go to
+              💻 Featured Projects
             </a>
             <ul class="dropdown-menu">
+              <li><a class="dropdown-item" href="#ml-projects">ML <span class="rounded-pill text-bg-warning p-1"> in progress</span></a></li>
               <li><a class="dropdown-item" href="#cloud-projects">DevOps</a></li>
               <li><a class="dropdown-item" href="#software-projects">Software</a></li>
 <!--              <li><hr class="dropdown-divider"></li>-->
-              <li><a class="dropdown-item" href="#uni-basics-projects">Basics</a></li>
+              <li><a class="dropdown-item" href="#uni-basics-projects">First Steps</a></li>
             </ul>
+          </li>
+
+          <li class="nav-item">
+            <a class="nav-link" aria-current="page" href="https://github.com/alexarlord-boop" target="_blank">GitHub</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="https://www.linkedin.com/in/aleksandr-petrunin-b4b720259/" target="_blank">LinkedIn</a>
           </li>
         </ul>
       </div>
